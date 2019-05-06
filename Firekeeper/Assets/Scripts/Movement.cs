8b58af9c428dfcf87ad17888110af58a0c30f568
RistoSK <risto.sardis.karhunen@gmail.com>
2019-05-06 15:32:56 +0300

    Player Movement & Resource Gathering

---
+++ Firekeeper/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Movement : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private Player player;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {       
+
+        if (gameObject.GetComponent<Resource>() != null)
+        {
+            Debug.Log("Lets chop em trees");
+            player.SetShouldGatherResource(true);
+        }
+        else
+        {
+            player.SetShouldGatherResource(false);
+        }
+
+        player.Move(eventData.pointerCurrentRaycast.worldPosition); 
+    }
+
+}
